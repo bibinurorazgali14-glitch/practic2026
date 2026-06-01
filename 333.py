5e5ca193632698dd3e976f0215d0815c8a41a2e0
bibinurorazgali14-glitch <bibinurorazgali14@gmail.com>
2026-06-01 09:07:01 +0300

    Add files via upload

---
+++ 333.py
@@ -0,0 +1,3 @@
+numbers = [45, 12, 78, 3, 25]
+numbers.sort()
+print("Сұрыпталған тізім:", numbers)
--- tapsyrma1.py
+++ tapsyrma1.py
@@ -1,12 +1,7 @@
+numbers = [12, 45, 7, 89, 23]
+max_number = numbers[0]
 
-arr = [34, 23, 12, 45, 78, 11, 89]
-n = len(arr)
-
-
-for i in range(n):
-    for j in range(0, n - i - 1):
-        if arr[j] > arr[j + 1]:
-
-            arr[j], arr[j + 1] = arr[j + 1], arr[j]
-
-print("Bubble Sort нәтижесі:", arr)
+for num in numbers:
+    if num > max_number:
+        max_number = num
+print("Ең үлкен элемент:", max_number)
